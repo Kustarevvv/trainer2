12f4198d265373055737c6f0b3eb611d34afabb6
Алексей Хитёв <hitev_ay@nlmk.com>
2025-02-22 10:07:33 +0300

    l4.2

--- examples/time_management_tool/src/main/java/ru/akhitev/dao/TaskJdbcDao.java
+++ examples/time_management_tool/src/main/java/ru/akhitev/dao/TaskJdbcDao.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 import ru.akhitev.domain.model.Task;
 import ru.akhitev.domain.repo.TaskRepository;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,62 +30,44 @@ public class TaskJdbcDao implements TaskRepository {
   private static final String DELETE_TASK_QUERY = """
           DELETE FROM tasks WHERE code=?
           """;
+  private final DataSource dataSource;
 
-  static {
-    try {
-      Class.forName("org.h2.Driver");
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public TaskJdbcDao() {
+  public TaskJdbcDao(DataSource dataSource) {
+    this.dataSource = dataSource;
     initDataBase();
   }
 
   public void initDataBase() {
-    Connection connection = null;
-    try {
-      connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement(DDL_QUERY);
+    try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(DDL_QUERY)) {
       statement.execute();
-      statement.close();
     } catch (SQLException e) {
       e.printStackTrace();
-    } finally {
-      closeConnection(connection);
     }
   }
 
   @Override
   public List<Task> findAll() {
-    Connection connection = null;
     List<Task> tasks = new ArrayList<>();
-    try {
-      connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement(FIND_ALL_QUERY);
-      ResultSet resultSet = statement.executeQuery();
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(FIND_ALL_QUERY);
+         ResultSet resultSet = statement.executeQuery();) {
       while (resultSet.next()) {
         Task task = new Task(resultSet.getString("code"),
                 resultSet.getString("title"));
         tasks.add(task);
       }
-      statement.close();
     } catch (SQLException e) {
       e.printStackTrace();
-    } finally {
-      closeConnection(connection);
     }
     return tasks;
   }
 
   @Override
   public Optional<Task> findByCode(String code) {
-    Connection connection = null;
     List<Task> tasks = new ArrayList<>();
-    try {
-      connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement(FIND_BY_CODE_QUERY);
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(FIND_BY_CODE_QUERY);) {
       statement.setString(1, code);
       ResultSet resultSet = statement.executeQuery();
       while (resultSet.next()) {
@@ -92,74 +75,43 @@ public class TaskJdbcDao implements TaskRepository {
                 resultSet.getString("title"));
         tasks.add(task);
       }
-      statement.close();
+      resultSet.close();
     } catch (SQLException e) {
       e.printStackTrace();
-    } finally {
-      closeConnection(connection);
     }
     return tasks.isEmpty() ? Optional.empty() : Optional.of(tasks.get(0));
   }
 
   @Override
   public void add(Task task) {
-    Connection connection = null;
-    try {
-      connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement(INSERT_TASK_QUERY);
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(INSERT_TASK_QUERY);){
       statement.setString(1, task.getCode());
       statement.setString(2, task.getTitle());
       statement.execute();
-      statement.close();
     } catch (SQLException e) {
       e.printStackTrace();
-    } finally {
-      closeConnection(connection);
     }
   }
 
   @Override
   public void update(Task task) {
-    Connection connection = null;
-    try {
-      connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement(UPDATE_TASK_QUERY);
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(UPDATE_TASK_QUERY);){
       statement.setString(1, task.getCode());
       statement.setString(2, task.getTitle());
       statement.execute();
-      statement.close();
     } catch (SQLException e) {
       e.printStackTrace();
-    } finally {
-      closeConnection(connection);
     }
   }
 
   @Override
   public void remove(String code) {
-    Connection connection = null;
-    try {
-      connection = getConnection();
-      PreparedStatement statement = connection.prepareStatement(DELETE_TASK_QUERY);
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(DELETE_TASK_QUERY);){
       statement.setString(1, code);
       statement.execute();
-      statement.close();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    } finally {
-      closeConnection(connection);
-    }
-  }
-
-  private Connection getConnection() throws SQLException {
-    return DriverManager.getConnection("jdbc:h2:mem:task;DB_CLOSE_DELAY=-1",
-            "admin", "admin");
-  }
-
-  private void closeConnection(Connection connection) {
-    if (connection == null) return;
-    try {
-      connection.close();
     } catch (SQLException e) {
       e.printStackTrace();
     }
